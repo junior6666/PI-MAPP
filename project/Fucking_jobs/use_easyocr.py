d5b1bf285084ba6b98a04faa752eb3a9de5848fc
pc <2642144249@qq.com>
2026-04-14 15:06:29 +0800

    chore(screenshots): 更新OCR测试截图文件路径
    - 更改了easyocr读取的测试图片文件名
    - 从quick_20260413_114303_173.png替换为ScreenShot_2026-04-13_140714_532.png

--- project/Fucking_jobs/use_easyocr.py
+++ project/Fucking_jobs/use_easyocr.py
@@ -1,7 +1,7 @@
 import easyocr
 
 reader = easyocr.Reader(['ch_sim', 'en'])  # 首次会自动下载模型
-result = reader.readtext('H:\pycharm_project\github_projects\PI-MAPP\project\Fucking_jobs\screenshots\quick_20260413_114303_173.png')
+result = reader.readtext('H:\pycharm_project\github_projects\PI-MAPP\project\Fucking_jobs\screenshots\ScreenShot_2026-04-13_140714_532.png')
 
 for bbox, text, prob in result:
     print(f'{text}')
